7992dce14eeed6a8bc29906f09632790630a8463
Yasmin Gomes Fabiano. <yasmingfabian@gmail.com>
2026-02-05 16:49:44 -0300

    Add files via upload

---
+++ 05-02-26/variaves.php
@@ -0,0 +1,17 @@
+<?php
+    $a = 10;
+    $b = 20;
+    $soma = $a + $b;
+    $multiplicacao= $a * $b;
+    $divisao = $a / $b;
+    $subtracao = $b - $a;
+
+
+    echo("A soma é: " .$soma. "\n");
+    echo("A multiplicação é: " .$multiplicacao."\n");
+    echo("A divisão é: " .$divisao."\n");
+    echo("A subtração é: " .$subtracao."\n");
+
+
+
+?>
